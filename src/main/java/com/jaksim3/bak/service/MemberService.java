b26c98377ca99ca7d6f7ae5fee07a04af5d84eb0
yoongyum <imshreg@naver.com>
2022-08-23 21:37:57 +0900

    feat: Create MemberService

---
+++ src/main/java/com/jaksim3/bak/service/MemberService.java
@@ -0,0 +1,23 @@
+package com.jaksim3.bak.service;
+
+import com.jaksim3.bak.conifg.SecurityUtil;
+import com.jaksim3.bak.domain.MemberRepository;
+import com.jaksim3.bak.web.dto.MemberResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public MemberResponseDto getMyInfoBySecurity() {
+        return memberRepository.findById(SecurityUtil.getCurrentMemberId())
+                .map(MemberResponseDto::of)
+                .orElseThrow(() -> new RuntimeException("로그인 유저 정보가 없습니다"));
+    }
+}
