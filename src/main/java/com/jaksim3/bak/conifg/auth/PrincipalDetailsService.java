76f92bf2458fd5632158e297e4822140d248eec4
yoongyum <imshreg@naver.com>
2022-08-23 21:38:19 +0900

    feat: Add createUserDetails and Edit loadUserByUsername

--- src/main/java/com/jaksim3/bak/conifg/auth/PrincipalDetailsService.java
+++ src/main/java/com/jaksim3/bak/conifg/auth/PrincipalDetailsService.java
@@ -1,11 +1,18 @@
 package com.jaksim3.bak.conifg.auth;
 
+import com.jaksim3.bak.domain.Member;
 import com.jaksim3.bak.domain.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
@@ -14,9 +21,21 @@ public class PrincipalDetailsService implements UserDetailsService {
     private final MemberRepository memberRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return memberRepository.findByUsername(username)
-                .map(PrincipalDetails::new)
-                .orElse(null);
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return memberRepository.findByEmail(userEmail)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException(userEmail + " 을 DB에서 찾지 못함!!"));
     }
+
+    private UserDetails createUserDetails(Member member) {
+        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(member.getAuthority().toString());
+
+        return new User(
+                String.valueOf(member.getId()),
+                member.getPassword(),
+                Collections.singleton(grantedAuthority)
+        );
+    }
+
 }
