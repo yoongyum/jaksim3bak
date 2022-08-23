91739d313a3fff20b003ea9c53ea44255fcad149
yoongyum <imshreg@naver.com>
2022-08-23 16:05:39 +0900

    Initial commit

---
+++ src/main/java/com/jaksim3/bak/BakApplication.java
@@ -0,0 +1,13 @@
+package com.jaksim3.bak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jaksim3/bak/BakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaksim3.bak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
