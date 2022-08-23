f71d9f79bd31e93d188ac8b7a3c729150b777ea4
yoongyum <imshreg@naver.com>
2022-08-23 20:13:29 +0900

    feat: Add QueryMethod as existByEmail

--- src/main/java/com/jaksim3/bak/domain/MemberRepository.java
+++ src/main/java/com/jaksim3/bak/domain/MemberRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByEmail(String userEmail);
+    Optional<Member> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
