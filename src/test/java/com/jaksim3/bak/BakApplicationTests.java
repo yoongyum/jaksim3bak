2a5b16c3d9c67ae5fa0fdeb99ee32a915c228465
yoongyum <imshreg@naver.com>
2022-08-24 16:51:27 +0900

    hotfix: @SpringbootTest remove

--- src/test/java/com/jaksim3/bak/BakApplicationTests.java
+++ src/test/java/com/jaksim3/bak/BakApplicationTests.java
@@ -1,9 +1,8 @@
 package com.jaksim3.bak;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BakApplicationTests {
 
     @Test
