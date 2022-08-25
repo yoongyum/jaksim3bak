40361fcccf138b58ba583445eebfda3921ef97bd
yoongyum <imshreg@naver.com>
2022-08-25 17:27:46 +0900

    feat: Add @ApiModelProperty

--- src/main/java/com/jaksim3/bak/web/dto/MemberRequestDto.java
+++ src/main/java/com/jaksim3/bak/web/dto/MemberRequestDto.java
@@ -2,6 +2,7 @@ package com.jaksim3.bak.web.dto;
 
 import com.jaksim3.bak.domain.Authority;
 import com.jaksim3.bak.domain.Member;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,10 +14,19 @@ import static lombok.AccessLevel.*;
 @NoArgsConstructor(access = PROTECTED)
 @Builder
 public class MemberRequestDto {
+    @ApiModelProperty(value = "회원가입 이메일", dataType = "String", required = true)
     private String email;
+
+    @ApiModelProperty(value = "비밀번호", dataType = "String", required = true)
     private String password;
+
+    @ApiModelProperty(value = "이름", dataType = "String", required = true)
     private String username;
+
+    @ApiModelProperty(value = "나이", dataType = "int", required = true)
     private int age;
+
+    @ApiModelProperty(value = "직업", dataType = "String", required = true)
     private String job;
 
     public Member toMember(PasswordEncoder passwordEncoder) {
