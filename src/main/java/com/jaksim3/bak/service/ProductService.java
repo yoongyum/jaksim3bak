187b0ce72232bc84a9226ed7d92399d83cee06bf
ho-jun97 <hojun9707@naver.com>
2022-08-26 20:07:52 +0900

    feat: Create ProductService Implement

--- src/main/java/com/jaksim3/bak/service/ProductService.java
+++ src/main/java/com/jaksim3/bak/service/ProductService.java
@@ -3,6 +3,8 @@ package com.jaksim3.bak.service;
 import com.jaksim3.bak.domain.Product;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 public interface ProductService {
-    ResponseEntity<Product> findAll();
+    ResponseEntity<List<Product>> findAll();
 }
