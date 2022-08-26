f6fdc0a008a6dff627e631b6718b7bfc51f3ff36
ho-jun97 <hojun9707@naver.com>
2022-08-26 22:00:59 +0900

    feat: Create ProductResponseDto

--- src/main/java/com/jaksim3/bak/service/ProductService.java
+++ src/main/java/com/jaksim3/bak/service/ProductService.java
@@ -1,10 +1,11 @@
 package com.jaksim3.bak.service;
 
 import com.jaksim3.bak.domain.Product;
+import com.jaksim3.bak.web.dto.ProductResponseDto;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface ProductService {
-    ResponseEntity<List<Product>> findAll();
+    ResponseEntity<List<ProductResponseDto>> findAll();
 }
