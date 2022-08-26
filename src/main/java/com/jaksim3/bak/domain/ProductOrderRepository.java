14750dd17979aceff651bfae0f219959b02497c9
yoongyum <imshreg@naver.com>
2022-08-27 00:53:32 +0900

    rename: Order -> ProductOrder

--- src/main/java/com/jaksim3/bak/domain/ProductOrderRepository.java
+++ src/main/java/com/jaksim3/bak/domain/ProductOrderRepository.java
@@ -2,5 +2,5 @@ package com.jaksim3.bak.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<ProductOrder, Long> {
+public interface ProductOrderRepository extends JpaRepository<ProductOrder, Long> {
 }
