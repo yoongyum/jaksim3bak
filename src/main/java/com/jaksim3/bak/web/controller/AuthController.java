8a301d5bdd1adaaaf0ffd4394e1864f62cd8c7a4
yoongyum <imshreg@naver.com>
2022-08-26 20:13:27 +0900

    fix: remove @ApiOperation

--- src/main/java/com/jaksim3/bak/web/controller/AuthController.java
+++ src/main/java/com/jaksim3/bak/web/controller/AuthController.java
@@ -4,9 +4,6 @@ import com.jaksim3.bak.service.AuthService;
 import com.jaksim3.bak.web.dto.MemberRequestDto;
 import com.jaksim3.bak.web.dto.MemberResponseDto;
 import com.jaksim3.bak.web.dto.TokenDto;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,11 +22,6 @@ public class AuthController {
         return ResponseEntity.ok(authService.signup(requestDto));
     }
 
-    @ApiOperation(value = "로그인 요청" , notes = "db에 등록된 현재 회원과 일치하면, 토큰 생성 후 리턴")
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "API 정상 작동"),
-            @ApiResponse(code = 500, message = "서버 에러"),
-    })
     @PostMapping("/login")
     public ResponseEntity<TokenDto> login(@RequestBody MemberRequestDto requestDto) {
         return ResponseEntity.ok(authService.login(requestDto));
