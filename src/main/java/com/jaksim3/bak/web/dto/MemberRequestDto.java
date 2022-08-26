06a3e4af19f327419471682b01610240dc3947e7
yoongyum <imshreg@naver.com>
2022-08-26 13:27:39 +0900

    refactor: remove access options

--- src/main/java/com/jaksim3/bak/web/dto/MemberRequestDto.java
+++ src/main/java/com/jaksim3/bak/web/dto/MemberRequestDto.java
@@ -2,15 +2,16 @@ package com.jaksim3.bak.web.dto;
 
 import com.jaksim3.bak.domain.Authority;
 import com.jaksim3.bak.domain.Member;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import static lombok.AccessLevel.*;
-
 @Getter
-@AllArgsConstructor(access = PROTECTED)
-@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class MemberRequestDto {
     private String email;
