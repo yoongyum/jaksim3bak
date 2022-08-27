f11bec2a6d756c2efc062e95029b0e354178b705
yoongyum <imshreg@naver.com>
2022-08-27 21:54:04 +0900

    fix: Add import

--- src/main/java/com/jaksim3/bak/web/dto/ProductOrderResponseDto.java
+++ src/main/java/com/jaksim3/bak/web/dto/ProductOrderResponseDto.java
@@ -1,7 +1,7 @@
 package com.jaksim3.bak.web.dto;
 
-import com.jaksim3.bak.domain.Product;
 import com.jaksim3.bak.domain.order.ProductOrder;
+import com.jaksim3.bak.domain.product.Product;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
