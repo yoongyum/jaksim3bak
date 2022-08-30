690d20291373598867178072c736e0ab798eef92
yoongyum <imshreg@naver.com>
2022-08-30 18:14:41 +0900

    feat: DEVELOPER -> OFFICE_WORKER

--- src/main/java/com/jaksim3/bak/domain/enums/Job.java
+++ src/main/java/com/jaksim3/bak/domain/enums/Job.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 public enum Job {
     STUDENT("학생", 5_000_000),
     PUBLIC_OFFICIAL("공무원", 500_000_000),
-    DEVELOPER("직장인", 350_000_000),
+    OFFICE_WORKER("직장인", 350_000_000),
     BUSINESSMAN("자영업", 1_050_000_000),
     HOUSEWIFE("주부", 26_500_000),
     UNEMPLOYED("무직", 20_000_000);
